28376b327690ef0a0a30ef901694734352bfdde6
Erik van Sebille <erikvansebille@gmail.com>
2024-05-02 11:59:06 +0200

    Removing redundant files and moving download_data to utils file

--- plasticparcels/utils.py
+++ plasticparcels/utils.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 import os
 import shapely as sh
 import json
+from urllib.request import urlretrieve
 
 
 def getclosest_ij(lats, lons, latpt, lonpt):
@@ -101,4 +102,63 @@ def load_settings(filename):
     """ A function to load a settings file in json format"""
     with open(filename, "r") as file:
         settings = json.load(file)
+    return settings
+
+
+def download_plasticparcels_dataset(dataset: str, settings, data_home=None):
+    """Load an example dataset from the parcels website.
+
+    This function provides quick access to a small number of example datasets
+    that are useful in documentation and testing in parcels.
+
+    Parameters
+    ----------
+    dataset : str
+        Name of the dataset to load.
+    data_home : pathlike, optional
+        The directory in which to cache data. If not specified, the value
+        of the ``PLASTICPARCELS_DATA`` environment variable, if any, is used.
+        Otherwise the default location is assigned by :func:`get_data_home`.
+
+    Returns
+    -------
+    dataset_folder : Path
+        Path to the folder containing the downloaded dataset files.
+    """
+
+    plasticparcels_data_files = {
+        "NEMO0083": [
+            (("release_maps", "coastal"), "coastal_population_MPW_NEMO0083.csv"),
+            (("release_maps", "rivers"), "river_emissions_NEMO0083.csv"),
+            (("release_maps", "fisheries"), "agg_data_fisheries_info.csv"),
+            (("unbeaching", "filename"), "land_current_NEMO0083.nc"),
+        ],
+    }
+
+    plasticparcels_data_url = "https://plasticadrift.science.uu.nl/plasticparcels/"
+
+    if dataset not in plasticparcels_data_files:
+        raise ValueError(
+            f"Dataset {dataset!r} not found. Available datasets are: "
+            ", ".join(plasticparcels_data_files.keys())
+        )
+
+    if data_home is None:
+        data_home = os.getcwd()
+    data_home = os.path.expanduser(data_home)
+    if not os.path.exists(data_home):
+        os.makedirs(data_home)
+
+    dataset_folder = os.path.join(data_home, dataset)
+
+    if not dataset_folder.exists():
+        dataset_folder.mkdir(parents=True)
+
+    for settings_path, filename in plasticparcels_data_files[dataset]:
+        filepath = dataset_folder / filename
+        settings[settings_path[0]][settings_path[1]] = filepath
+        if not filepath.exists():
+            url = f"{plasticparcels_data_url}/{dataset}/{filename}"
+            urlretrieve(url, str(filepath))
+
     return settings
